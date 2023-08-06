5b9a1f1a9edac6c74b8e8caf15c8c428af147fe2
Fennia <fweidne2@hs-mittweida.de>
2023-08-06 21:05:10 +0200

    UI Start

--- ExoPrototype2/Assets/Shader/UI/FieldCollision.cs
+++ ExoPrototype2/Assets/Shader/UI/FieldCollision.cs
@@ -2,14 +2,24 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class FieldCollision : MonoBehaviour
+public class FieldCollision : MonoBehaviour, IPointerClickHandler
 {
    private Material _material;
+   [SerializeField] private bool isUI;
    
     void Start()
    {
-      _material = GetComponent<MeshRenderer>().material;
+       if (isUI)
+      {
+          _material = GetComponent<Image>().material;
+      }
+      else
+      {
+          _material = GetComponent<MeshRenderer>().material;
+      }
    }
    
     void OnCollisionEnter(Collision co)
@@ -25,12 +35,16 @@ public class FieldCollision : MonoBehaviour
     {
         yield return new WaitForSeconds(2);
         _material.SetVector("_SpherePos", new Vector3(0, 0, 0));
+        if (isUI)
+        {
+            _material.SetVector("_SpherePos", new Vector3(575.78f, 0, 0)); 
+        }
         _material.SetFloat("_Hardness", 1);
     }
 
     private void Update()
     {
-        /*if (Input.GetMouseButtonDown(0))
+        /*if (Input.GetMouseButtonDown(0) && isUI)
         {
             CastClickRay();
         }*/
@@ -51,5 +65,21 @@ public class FieldCollision : MonoBehaviour
             StartCoroutine(Wait());
             this.gameObject.GetComponent<Dissolve>().isdissolving = true;
         }
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Vector2 clickPosition = eventData.position;
+
+        // You can now use the clickPosition to perform actions based on the click position
+        // For example, you can convert it to world space if needed:
+        //Vector3 worldClickPosition = Camera.main.ScreenToWorldPoint(clickPosition);
+        
+       Debug.Log("Click on obj");
+       _material.SetFloat("_Hardness", 0);
+       _material.SetVector("_SpherePos", clickPosition);
+       // Give me code to wait for two seconds
+       StartCoroutine(Wait());
+
     }
 }
